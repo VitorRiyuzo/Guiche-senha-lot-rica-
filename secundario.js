0cee78fe2fa4ffd07166e18b63e8a7e4e4d84cc9
VitorRiyuzo <vitor_riyuzo@hotmail.com>
2015-09-28 22:30:30 -0300

    enviando arquivo

---
+++ secundario.js
@@ -0,0 +1,65 @@
+function retira_senha(){
+	document.getElementById("retira").style.visibility = 'visible';
+}
+
+
+$(
+function (){
+	$("#recarga").click(
+	function(){
+		$(".senha_recarga").show();
+			}
+		);
+		}
+		);
+		
+
+$(
+function (){
+	$("#jogo").click(
+	function(){
+		$(".senha_jogo").show();
+			}
+		);
+		}
+		);
+		
+
+$(
+function (){
+	$("#pagamento").click(
+	function(){
+		$(".senha_pagamento").show();
+			}
+		);
+		}
+		);
+		
+
+$(
+function (){
+	$("#saque").click(
+	function(){
+		$(".senha_saque").show();
+			}
+		);
+		}
+		);
+		
+
+$(
+function (){
+	$("#variado").click(
+	function(){
+		$(".senha_variado").show();
+			}
+		);
+		}
+		);
+		
+
+
+function ativa_som(){
+	document.getElementById("som_maquina").play()}
+
+
